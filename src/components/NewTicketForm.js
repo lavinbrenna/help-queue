010215f858476daec197237dbe12a701222cdea8
Brenna Lavin <lavinbrenna@gmail.com>
2022-04-24 09:28:55 -0700

    add reducers and tests for reducers

--- src/components/NewTicketForm.js
+++ src/components/NewTicketForm.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { v4 } from 'uuid';
 import PropTypes from 'prop-types';
+import ReusableForm from "./ReusableForm";
 
 
 function NewTicketForm(props){
@@ -10,20 +11,7 @@ function NewTicketForm(props){
   }
   return (
     <React.Fragment>
-      <form onSubmit ={handleNewTicketFormSubmission}>
-        <input
-          type='text'
-          name='names'
-          placeholder ='Pair Names'/>
-        <input
-          type='text'
-          name='location'
-          placeholder ='Location'/>
-          <textarea
-          name='issue'
-          placeholder ='Describe your issue.'/>
-          <button type='submit'>Help!</button>
-        </form>
+      <ReusableForm formSubmissionHandler = {handleNewTicketFormSubmission} buttonText = "Help!"/>
     </React.Fragment>
   );
 }
@@ -31,3 +19,7 @@ NewTicketForm.propTypes = {
   onNewTicketCreation: PropTypes.func
 };
 export default NewTicketForm;
+
+
+
+
